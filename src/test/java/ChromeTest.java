2728bcfe132b9ed4c240ed01cb344614020da78b
Denis <dzavalskiy@gmail.com>
2023-05-24 09:42:40 +0400

    One commit

--- src/test/java/ChromeTest.java
+++ src/test/java/ChromeTest.java
@@ -2,10 +2,13 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ChromeTest {
 
@@ -27,8 +30,15 @@ class ChromeTest {
     }
 
     @Test
-    void test() {
-        // Your test logic here
+    void test(){
+        driver.get("http://localhost:9999");
+        driver.findElements(By.tagName("input")).get(0).sendKeys("Вадченко Виктор");
+        driver.findElements(By.tagName("input")).get(1).sendKeys("+79270000000");
+        driver.findElement(By.tagName("label")).click();
+        driver.findElement(By.tagName("button")).click();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.tagName("p")).getText().trim();
+        assertEquals(expected, actual);
     }
 
 }
