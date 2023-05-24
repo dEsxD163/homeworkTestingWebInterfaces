d7b7e5a0aa1b58900d6f4b9de1044c7807542219
Denis <dzavalskiy@gmail.com>
2023-05-24 11:17:45 +0400

    Four commit

--- src/test/java/ChromeTest.java
+++ src/test/java/ChromeTest.java
@@ -1,14 +1,11 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ChromeTest {
 
@@ -21,7 +18,12 @@ class ChromeTest {
 
     @BeforeEach
     void setup() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999/");
     }
 
     @AfterEach
@@ -31,15 +33,13 @@ class ChromeTest {
     }
 
     @Test
-    void test(){
-        driver.get("http://localhost:9999");
-        driver.findElements(By.tagName("input")).get(0).sendKeys("Вадченко Виктор");
-        driver.findElements(By.tagName("input")).get(1).sendKeys("+79270000000");
-        driver.findElement(By.tagName("label")).click();
-        driver.findElement(By.tagName("button")).click();
+    void test() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Завальский Денис");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79272662361");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button__text")).click();
         String expected = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        String actual = driver.findElement(By.tagName("p")).getText();
-        assertEquals(expected, actual);
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        Assertions.assertEquals(expected, actual);
     }
-
 }
